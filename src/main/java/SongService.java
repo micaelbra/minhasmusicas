f26a41b6f4d94991169541393cd15796b004ec13
miquimbas <micaelcruz89@gmail.com>
2019-10-27 11:50:11 -0300

    Refatoração

--- src/main/java/SongService.java
+++ src/main/java/SongService.java
@@ -1,3 +1,5 @@
+import static java.util.Arrays.asList;
+
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
@@ -6,19 +8,38 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class SongService {
 
 	private static final String MP3 = ".mp3";
-	String songsFilePath = PropertiesService.getProperty(PropertiesService.REPOSITORY_FILE_PATH);
+	private static final int SIM = 1;
+	public static final String SEPARATOR = ";";
+	
+	private String songsFilePath = PropertiesService.getProperty(PropertiesService.REPOSITORY_FILE_PATH);
+	
+	public List<String> getRepositorySongs() {
+		try {
+			String repositorySongs = getFileContent();
+			String[] splittedSongs = repositorySongs.split(SEPARATOR);
+			
+			if (splittedSongs.length > 0) {
+				return asList(splittedSongs);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return new ArrayList<>();
+	}
 
-	public List<String> getSongsNames(String path) {
+	public List<String> getLocalSongsNames(String path) {
 		File songsFolder = new File(path);
 		List<String> songsNames = new ArrayList<>();
 		
 		for (File file : songsFolder.listFiles()) {
 			if (file.isDirectory()) {
-				songsNames.addAll(getSongsNames(file.getPath()));
+				songsNames.addAll(getLocalSongsNames(file.getPath()));
 			}
 			
 			if(file.getName().endsWith(MP3)) {
@@ -44,5 +65,62 @@ public class SongService {
 		}
 		
 		return new String(Files.readAllBytes(Paths.get(songsFilePath)));
+	}
+	
+	public List<String> getDiffSongs(List<String> songList1, List<String> songList2) {
+		List<String> diffSongs = new ArrayList<>();
+		
+		for (String song1 : songList1) {
+			String songTemp = "";
+			
+			for (String song2 : songList2) {
+				if (song1.equals(song2)) {
+					songTemp = song1;
+				}
+			}
+			
+			if (songTemp.isEmpty()) {
+				diffSongs.add(song1);
+			}
+		}
+		return diffSongs;
+	}
+	
+	@SuppressWarnings("resource")
+	public void addSongsToRepositoryFile(List<String> diffSongsRepository) {
+		if (diffSongsRepository.size() > 0) {
+			System.out.println("Deseja adicionar as musicas ao repositório?");
+			System.out.println("1-Sim ou 2-Não");
+			Scanner sc = new Scanner(System.in);
+			int input = sc.nextInt();
+			
+			if (input == SIM) {
+				String formatedSongsNames = "";
+				
+				for (String song : diffSongsRepository) {
+					formatedSongsNames += song + SongService.SEPARATOR;
+				}
+				
+				try {
+					appendContentToFile(formatedSongsNames);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	public List<String> getDiffRepositorySongs() {
+		List<String> localSongs = getLocalSongsNames(PropertiesService.getProperty("local.folder.path"));
+		List<String> repositorySongs = getRepositorySongs(); 
+
+		return getDiffSongs(localSongs, repositorySongs);
+	}
+
+	public List<String> getDiffLocalSongs() {
+		List<String> localSongs = getLocalSongsNames(PropertiesService.getProperty("local.folder.path"));
+		List<String> repositorySongs = getRepositorySongs(); 
+
+		return getDiffSongs(repositorySongs, localSongs);
 	}
 }
--- src/main/java/SongsComparator.java
+++ src/main/java/SongsComparator.java
@@ -1,22 +1,12 @@
-import static java.util.Arrays.asList;
-
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class SongsComparator {
 
-	private static final int SIM = 1;
-	private static final String SEPARATOR = ";";
 	private static SongService songService = new SongService();
 	
 	public static void main(String[] args) {
-		List<String> localSongs = songService.getSongsNames(PropertiesService.getProperty("local.folder.path"));
-		List<String> repositorySongs = getRepositorySongs(); 
-
-		List<String> diffSongsRepository = getDiffSongs(localSongs, repositorySongs);
-		List<String> diffSongsLocal = getDiffSongs(repositorySongs, localSongs);
+		List<String> diffSongsRepository = songService.getDiffRepositorySongs();
+		List<String> diffSongsLocal = songService.getDiffLocalSongs();
 		
 		System.out.println("MUSICAS QUE VOCE NÃO TEM LOCALMENTE");
 		System.out.println();
@@ -34,64 +24,6 @@ public class SongsComparator {
 			System.out.println(song);
 		}
 		
-		addSongsToRepositoryFile(diffSongsRepository);
+		songService.addSongsToRepositoryFile(diffSongsRepository);
 	}
-
-	private static void addSongsToRepositoryFile(List<String> diffSongsRepository) {
-		if (diffSongsRepository.size() > 0) {
-			System.out.println("Deseja adicionar as musicas ao repositório?");
-			System.out.println("1-Sim ou 2-Não");
-			Scanner sc = new Scanner(System.in);
-			int input = sc.nextInt();
-			
-			if (input == SIM) {
-				String formatedSongsNames = "";
-				
-				for (String song : diffSongsRepository) {
-					formatedSongsNames += song + SEPARATOR;
-				}
-				
-				try {
-					songService.appendContentToFile(formatedSongsNames);
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-	}
-
-	private static List<String> getDiffSongs(List<String> songList1, List<String> songList2) {
-		List<String> diffSongs = new ArrayList<>();
-		
-		for (String song1 : songList1) {
-			String songTemp = "";
-			
-			for (String song2 : songList2) {
-				if (song1.equals(song2)) {
-					songTemp = song1;
-				}
-			}
-			
-			if (songTemp.isEmpty()) {
-				diffSongs.add(song1);
-			}
-		}
-		return diffSongs;
-	}
-
-	private static List<String> getRepositorySongs() {
-		try {
-			String repositorySongs = songService.getFileContent();
-			String[] splittedSongs = repositorySongs.split(SEPARATOR);
-			
-			if (splittedSongs.length > 0) {
-				return asList(splittedSongs);
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		return new ArrayList<>();
-	}
-
 }
